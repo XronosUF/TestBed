db939b8fabe514ea60541247120e7721c088e3fc
XronosUF <XronosUF@gmail.com>
2016-12-05 16:05:55 -0500

    Testing interactives

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 x = Math.random();
 console.log(x);
 console.log("This is a test!");
-
+define(['parameters'], function(parameters) {
+  console.log( "x = ", parameters.x );
+  console.log( "y = ", parameters.y );
+});
+parameters.()
