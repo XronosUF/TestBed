9648d0c6321f9301bb2aedbaf86054563aee734e
Jason <jnowell@ufl.edu>
2020-01-28 13:49:15 -0500

    Reappropriate a testbed repo.

--- test.js
+++
@@ -1,8 +0,0 @@
-x = Math.random();
-console.log(x);
-console.log("This is a test!");
-define(['parameters'], function(parameters) {
-  console.log( "x = ", parameters.x );
-  console.log( "y = ", parameters.y );
-});
-parameters.()
